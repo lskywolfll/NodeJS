53baa40800069496a538d04a37f4f8d7ec869a01
skywolfll <renealejandrosanchezmorales@gmail.com>
2019-10-11 15:10:36 -0300

    Uso del fileSystem de nodejs y crear

--- 03-BASES-NODE/app.js
+++ 03-BASES-NODE/app.js
@@ -1,10 +1,24 @@
-let base = 3;
+// paquetes
+const fs = require('fs');
+
+
+let base = 5;
+let data = ``;
 
 function multiplicacion(base, limite) {
     
     for(let i = 1; i <= limite; i++){
-        console.log(`${base} * ${i} = ${base * i}`);
+        data += `${base} * ${i} = ${base * i}\n`;
     }
 }
 
 multiplicacion(base, 10);
+
+// 1- Nombre del archivo y extencion a crear, 2- el contenido que tendra 3- callback
+// en el nombre del archivo nosotros podemos indicar una carpeta carpeta/nombre.extencion
+fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+
+  if (err) throw err;
+
+  console.log(`El archivo con la tabla del ${base} ha sido creada`);
+});
