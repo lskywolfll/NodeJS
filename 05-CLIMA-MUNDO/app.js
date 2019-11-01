00acbcb611d955a3e4573955f99af3df3bf63b7a
lskywolfll <renealejandrosanchezmorales@gmail.com>
2019-11-01 20:22:22 -0300

    Se completa el desafio de 3 formas :)

--- 05-CLIMA-MUNDO/app.js
+++ 05-CLIMA-MUNDO/app.js
@@ -1,6 +1,6 @@
 
-const {climaLugar} = require('./lugar/lugar');
-const {getClima} = require('./clima/clima');
+const { climaLugar } = require('./lugar/lugar');
+const { getClima } = require('./clima/clima');
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -11,11 +11,48 @@ const argv = require('yargs').options({
 
 console.log(argv.direccion);
 
-climaLugar(argv.direccion)
+// climaLugar(argv.direccion)
+//     .then(resp => {
+//         console.log(`Ciudad: ${resp.direction}`);
+//         console.log(`Latitud: ${resp.latitud}`);
+//         console.log(`Longitud: ${resp.longitud}`);
+//         // console.log(`Grados: ${resp.grados}`);
+//     })
+//     .catch(err => console.log(err));
+
+
+// Desafio!
+// Crear una funcion que reciba una direccion y este me envie una mensaje por consola si paso bien: El clima de direccion es de n° grados y si no se pudo obtener se imprime: No se pudo determinar el cliam de direccion
+
+const getInfo = async (direccion) => {
+    const coordenadas = await climaLugar(direccion);
+    const temperatura = await getClima(coordenadas.latitud, coordenadas.longitud);
+
+    if (!temperatura || !coordenadas) {
+        return new Error(`No se pudo determinar el clima de ${direccion}`);
+    }
+
+    return {
+        coordenadas,
+        temperatura
+    }
+
+    // 2 forma
+    // try {
+    //     const coordenadas = await climaLugar(direccion);
+    //     const temperatura = await getClima(lugar.latitud, lugar.longitud);
+    //     return `El clima de ${coordenadas.direction} es de ${temperatura}`
+    // } catch (e) {
+    //     return `No se pudo determinar el clima de ${direccion}`
+    // }
+}
+
+getInfo(argv.direccion)
     .then(resp => {
-        console.log(`Ciudad: ${resp.direction}`);
-        console.log(`Latitud: ${resp.latitud}`);
-        console.log(`Longitud: ${resp.longitud}`);
-        console.log(`Grados: ${resp.grados}`);
+        console.log(`El clima de ${resp.coordenadas.direction} es de ${resp.temperatura}`);
     })
     .catch(err => console.log(err));
+// 2 forma
+// getInfo(argv.direccion)
+//     .then(console.log)
+//     .catch(console.log);
