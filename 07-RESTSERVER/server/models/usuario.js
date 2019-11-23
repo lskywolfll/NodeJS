ea3ce620becf8035dffcc44144f3b570500113d7
lskywolfll <renealejandrosanchezmorales@gmail.com>
2019-11-23 16:48:49 -0300

    Se agregan nuevas validaciones, se agrega el uso del enum para dictar solamente ciertos valores no cualquier cosa

--- 07-RESTSERVER/server/models/usuario.js
+++ 07-RESTSERVER/server/models/usuario.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator'); 
 
 // Crear schema en mongo para que sea el objeto(Tabla) de la base de datos por su estructura
 let Schema = mongoose.Schema;
+
+const rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+};
+
 // Se crean las propiedades del objeto de la entidad que tenemos en nuestra base de datos de las tabla ala cual estemos haciendo referencia
 let usuarioSchema = new Schema({
     // En las propiedades nosotros podemos establecer restricciones en su uso, ya sea tipo de dato y campo obligatorio
@@ -11,7 +18,9 @@ let usuarioSchema = new Schema({
     },
     email: {
         type: String,
-        required: [true, 'El correo es necesario']
+        // Al establecer un dato unico que no se puede repetir en la BD de mongo, solo agregamos el unique para que lo valide y tambien tener en cuenta reiniciar el servidor para que pueda tomar los cambios ya que a veces no los toma al tenerlo corriendo con nodemon
+        unique: true,
+        required: [true, 'El correo es necesario'],
     },
     password: {
         type: String,
@@ -23,7 +32,9 @@ let usuarioSchema = new Schema({
     },
     role:{
         type: String,
-        default: 'USER_ROLE'
+        default: 'USER_ROLE',
+        // Enum => Para solamente establecer ciertos valores para aceptar dentro de un array(cajita) y un message(Error) donde nosotros estableceremos como nos devuelve el error con su definicion respectiva.
+        enum: rolesValidos
     },
     estado:{
         type: Boolean,
@@ -33,6 +44,10 @@ let usuarioSchema = new Schema({
         type: Boolean,
         default: false
     }
+});
+// Agregamos una funcionalidad al esquema donde le agregamos un superpoder, en este punto usarmos la paqueteria de validacion que nos devolver el error completo y mejor estructurado, pero de igual manera se puede agregar otro parametros en el cual lo invoquemos como objeto y pongamos la propiedad de message y dentro de su contenido ponemos el {PATH} PATH en este punto seria el error encontrado por el cual no paso al validacion donde podemos agregar otro contenido despues de enviar el error.
+usuarioSchema.plugin(uniqueValidator, {
+    message: '{PATH} debe de ser único'
 });
 
 // Al crear un modelo de datos debemos indicar primero el nombre clave que tendra y luego poner el schema en el cual se basa
